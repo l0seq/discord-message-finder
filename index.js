cd143375188d3f196e1fa7aa31efa41eb2f49840
Loseq <loseq@proton.me>
2023-08-14 12:03:09 +0200

    Remove master keys and enhance error catching

--- index.js
+++ index.js
@@ -3,10 +3,8 @@ const fs = require("fs")
 const { parse } = require("csv-parse")
 const messagesFolder = fs.readdirSync(path.join(__dirname, 'package', 'messages'))
 const { MeiliSearch } = require('meilisearch')
-const { apiKey } = require("./config.json")
 const search = new MeiliSearch({
     host: 'http://localhost:7700',
-    apiKey: apiKey
 })
 
 messagesFolder.forEach((f) => {
@@ -19,31 +17,41 @@ messagesFolder.forEach((f) => {
             columns: ['id', 'timestamp', 'content'],
             record_delimiter: "\n",
             relax_column_count: true
-        }, (err, rec) => {
-            rec.shift()
-            if (channelFile.type == 0 || channelFile.type == 11) {
-                rec.forEach((r) => {
-                    data.push({
-                        guild: `[${channelFile.type}] ${channelFile.guild?.name} (${channelFile.guild?.id})`,
-                        channel: `${channelFile.name} (${channelFile.id})`,
-                        messageId: r.id,
-                        messageTime: r.timestamp,
-                        messageContent: r.content,
-                    })
-                })
+        }, async (err, rec) => {
+            if(!err) {
+                try {
+                    rec.shift()
+                    if (channelFile.type == 0 || channelFile.type == 11) {
+                        rec.forEach((r) => {
+                            data.push({
+                                guild: `[${channelFile.type}] ${channelFile.guild?.name} (${channelFile.guild?.id})`,
+                                channel: `${channelFile.name} (${channelFile.id})`,
+                                message_id: r.id,
+                                message_time: r.timestamp,
+                                message_content: r.content,
+                            })
+                        })
+                    }
+                    else {
+                        rec.forEach((r) => {
+                            data.push({
+                                recipients: `${channelFile.recipients?.[0]} & ${channelFile.recipients?.[1]}`,
+                                message_id: r.id,
+                                message_time: r.timestamp,
+                                message_content: r.content,
+                            })
+                        })
+                    }
+                    const s = await search.index('messages').addDocuments(data)
+                    console.log(s)
+                }
+                catch(e) {
+                    console.error(e)
+                }
             }
             else {
-                rec.forEach((r) => {
-                    data.push({
-                        recipients: `${channelFile.recipients?.[0]} & ${channelFile.recipients?.[1]}`,
-                        messageId: r.id,
-                        messageTime: r.timestamp,
-                        messageContent: r.content,
-                    })
-                })
+                console.log("[ERROR] Parsing Error (CSV)")
             }
-            search.index('messages').addDocuments(data)
-            .then((res) => console.log(res))          
         })
     }
 })
